b8a0a5473a179e19997d61bd5bb3ac98a623d5da
iamapusaha <apusaha392@gmail.com>
2023-10-14 23:35:06 +0600

    added coffee form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -2,8 +2,75 @@
 
 const AddCoffee = () => {
     return (
-        <div>
-
+        <div className="container mx-auto my-6 bg-[#F4F3F0] p-5 rounded">
+            <h1 className="text-5xl text-center my-2">Add New Coffee</h1>
+            <form>
+                <div className="md:flex gap-3 px-2 md:px-1 mb-6">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Your Name</span>
+                        </label>
+                        <label className="input-group ">
+                            <input type="text" name="name" placeholder="name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Chef</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="chef" placeholder="chef" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <div className="md:flex gap-3 px-2 md:px-1 mb-6">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Supplier</span>
+                        </label>
+                        <label className="input-group ">
+                            <input type="text" name="supplier" placeholder="Supplier" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Taste</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="taste" placeholder="Taste" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <div className="md:flex gap-3 px-2 md:px-1 mb-6">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <label className="input-group ">
+                            <input type="text" name="category" placeholder="Categoryr" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="details" placeholder="Details" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <div className="px-2 md:px-1 mb-6">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Photo</span>
+                        </label>
+                        <label className="input-group ">
+                            <input type="text" name="photo" placeholder="Enter photo URL" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <input className="btn btn-block btn-neutral" type="submit" value="Add Coffee" />
+            </form>
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
+        <div className="flex gap-3">
             <Link to='/'>Home</Link>
             <Link to='/addcoffee'>Add Coffee</Link>
             <Link to='/updatecoffee'>Upodate coffee</Link>
