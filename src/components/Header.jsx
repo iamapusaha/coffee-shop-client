fac0a38e8f5e24f7cc153b24f14d9baea973cc62
iamapusaha <apusaha392@gmail.com>
2023-10-16 21:55:10 +0600

    get data from signup  form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,8 @@ const Header = () => {
         <div className="flex gap-3 container mx-auto my-5">
             <Link to='/'>Home</Link>
             <Link to='/addcoffee'>Add Coffee</Link>
+            <Link to='/signin'>SignIn</Link>
+            <Link to='/signUp'>signUp</Link>
         </div>
     );
 };
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,9 +2,42 @@
 // import PropTypes from 'prop-types';
 
 const SignUp = () => {
+    const handleSignUp = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
     return (
-        <div>
-
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign Up!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
